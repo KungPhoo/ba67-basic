2f37115cc00769b325418d3a08eb8a20e4569d12
KungPhoo <gf@glbasic.com>
2025-03-21 05:53:56 +0100

    -startx first try

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -e
 
 echo update system and install libraries
 sudo apt-get update
-sudo apt install git g++ cmake xserver-xorg xinit openbox pulseaudio libpthread-stubs0-dev libasound2-dev libx11-dev libglew-dev -y
+sudo apt install git g++ cmake xserver-xorg xinit x11-xserver-utils openbox pulseaudio libpthread-stubs0-dev libasound2-dev libx11-dev libglew-dev -y
 
 if [ ! -e /usr/lib/aarch64-linux-gnu/libpthread.so ]; then
     sudo ln -s /usr/lib/aarch64-linux-gnu/libpthread.so.0 /usr/lib/aarch64-linux-gnu/libpthread.so
@@ -26,9 +26,25 @@ git stash
 echo update sources
 git fetch origin
 git reset --hard origin/main
+git pull origin main
 
 echo updating from sources...
 chmod +x build.sh
 ./build.sh
 
+if [ -f ~/ba67/BA67 ]; then
+    echo detect resolution
+    xrandr
+    echo RESOLUTION = $(xrandr | grep '*' | awk '{print $1}')
+    echo OUTPUT = $(xrandr | grep " connected" | awk '{print $1}')
+
+    # Automatically detect the screen resolution
+    RESOLUTION=$(xrandr | grep '*' | awk '{print $1}')
+    OUTPUT=$(xrandr | grep " connected" | awk '{print $1}')
+    
+    # Apply the detected resolution
+    xrandr --output $OUTPUT --mode $RESOLUTION
+
+    startx ~/ba67/BA67 --fullscreen --video opengl &
+fi
 
