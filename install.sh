28a208ee0759f0b4dd1032e3a569c7692f9a150e
KungPhoo <gf@glbasic.com>
2025-03-22 06:57:53 +0100

    -installer

--- install.sh
+++ install.sh
@@ -21,13 +21,13 @@ if [ -f ~/ba67/bin/BA67 ]; then
 
     echo installing autostart of BA67 for openbox
     mkdir -p ~/.config/openbox
-    echo ~/ba67/bin/BA67 --fullscreen > ~/.config/openbox/autostart
+    echo '~/ba67/bin/BA67 --fullscreen' > ~/.config/openbox/autostart
     chmod +x ~/.config/openbox/autostart
 
     echo installing autostart of openbox in .bash_profile
-    echo if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then > ~/.bash_profile
-    echo     startx /usr/bin/openbox-session &                    > ~/.bash_profile
-    echo fi                                                       > ~/.bash_profile
+    echo 'if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then' > ~/.bash_profile
+    echo '    startx /usr/bin/openbox-session &'                    > ~/.bash_profile
+    echo 'fi'                                                       > ~/.bash_profile
     
     # startx /usr/bin/openbox-session &
 fi
