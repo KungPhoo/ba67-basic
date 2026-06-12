ce4d011cfdf840286a2943fcb9d0e371e50cac63
KungPhoo <gf@glbasic.com>
2026-06-12 22:27:55 +0200

    -and again

--- src/screen_buffer.cpp
+++ src/screen_buffer.cpp
@@ -249,11 +249,11 @@ std::string& ScreenBuffer::updateScreenTerminal(std::function<char32_t(char32_t)
         // 14 (light cyan) use reused as a light gray.
         static const std::vector<int> c64term = { 0, 15, 1, 6, 5, 2, 4, 11,    3, 13 /*lt purple=brown*/, 9, 8, 7, 10, 12, 14 /*lt.cyan=lt gray*/ };
         for (int i = 0; i < 15; ++i) {
-            uint32_t c = palette[c64term[i]];
+            uint32_t c = palette[i];
             int r = (c >> 16) & 0xff;
             int g = (c >> 8) & 0xff;
             int b = (c >> 0) & 0xff;
-            printf(ESC "]P%.1X%.2X%.2X%.2X\n", i, r, g, b);
+            printf(ESC "]P%.1X%.2X%.2X%.2X\n", c64term[i], r, g, b);
         }
         // https://en.wikipedia.org/wiki/ANSI_escape_code
         // only two "gray" in this mode
